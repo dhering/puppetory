d764095dba3ffd260be9f7c3279c35bcb7f42677
heringd <dennis.hering@nexum.de>
2015-05-23 23:20:24 +0200

    add some commets

--- src/main/java/org/puppetory/model/api/Collection.java
+++ src/main/java/org/puppetory/model/api/Collection.java
@@ -2,7 +2,20 @@ package org.puppetory.model.api;
 
 import java.util.List;
 
+/**
+ * Collections are a subset of the {@link Inventory} which is a filtered list of {@link Component}s.
+ */
 public interface Collection {
+    /**
+     *
+     * @return empty list, if no results
+     */
     public List<Component> getComponents();
+
+    /**
+     *
+     *
+     * @return used filter
+     */
     public Filter getFilter();
 }
