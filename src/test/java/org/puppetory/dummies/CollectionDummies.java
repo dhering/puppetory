22c3e7e35440c879ee0cc329d765d35ceeb8457e
heringd <dennis.hering@nexum.de>
2015-05-23 23:38:37 +0200

    add some dummy stuff

--- src/test/java/org/puppetory/dummies/CollectionDummies.java
+++ src/test/java/org/puppetory/dummies/CollectionDummies.java
@@ -1,28 +1,31 @@
 package org.puppetory.dummies;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.puppetory.model.api.Collection;
 import org.puppetory.model.api.Component;
-import org.puppetory.model.api.Fact;
 import org.puppetory.model.impl.CollectionImpl;
 import org.puppetory.model.impl.ComponentImpl;
 import org.puppetory.model.impl.FilterImpl;
-import org.puppetory.model.impl.TextualFact;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CollectionDummies {
 	
 	public static Collection getSimpleCollection(){
-		
-		Component component = new ComponentImpl(Arrays.asList(new Fact[] {
-				new TextualFact("name","simplecollection"),
-				new TextualFact("test","fact")
-		}));
+
+        Component component = new ComponentImpl(FactDummies.getTwoFacts());
 		
 		List<Component> components = Arrays.asList(new Component[]{component});
 		
 		return new CollectionImpl(components, new FilterImpl());
 	}
 
+    public static Collection getCollectionWithStructuredFact(){
+
+        Component component = new ComponentImpl(FactDummies.getStructuredFacts());
+
+        List<Component> components = Arrays.asList(new Component[]{component});
+
+        return new CollectionImpl(components, new FilterImpl());
+    }
 }
