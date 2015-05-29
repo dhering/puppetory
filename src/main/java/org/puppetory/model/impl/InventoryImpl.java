0e91e072b8f5efeadb452b5654dde9bafb5e2f61
heringd <dennis.hering@nexum.de>
2015-05-30 00:01:01 +0200

    add insert and upsert methods

--- src/main/java/org/puppetory/model/impl/InventoryImpl.java
+++ src/main/java/org/puppetory/model/impl/InventoryImpl.java
@@ -2,6 +2,7 @@ package org.puppetory.model.impl;
 
 import org.puppetory.data.api.Repository;
 import org.puppetory.model.api.Collection;
+import org.puppetory.model.api.Component;
 import org.puppetory.model.api.Filter;
 import org.puppetory.model.api.Inventory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,15 @@ public class InventoryImpl implements Inventory {
 	public Collection find(String collection, Filter filter) {
 		return repository.find(collection, filter);
 	}
+
+    @Override
+    public void insert(String collectionName, Component component) {
+        repository.insert(collectionName, component);
+    }
+
+    @Override
+    public void upsert(String collectionName, Component component, Filter filter) {
+        repository.upsert(collectionName, component, filter);
+    }
 
 }
