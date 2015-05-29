ce2c6c0b10b1a85529575b5b65bfe137f63fc572
heringd <dennis.hering@nexum.de>
2015-05-30 00:01:57 +0200

    sort toString output

--- src/main/java/org/puppetory/model/impl/ListedFact.java
+++ src/main/java/org/puppetory/model/impl/ListedFact.java
@@ -1,7 +1,9 @@
 package org.puppetory.model.impl;
 
+import org.puppetory.model.FactListComparator;
 import org.puppetory.model.api.Fact;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ListedFact extends StructuredFact {
@@ -17,6 +19,7 @@ public class ListedFact extends StructuredFact {
 		String out = "";
 
         List<Fact> facts = getFacts();
+        Collections.sort(facts, FactListComparator.getInstance());
 
         for(int i=0; i<facts.size(); i++){
 			Fact fact = facts.get(i);
