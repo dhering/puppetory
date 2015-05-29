7f29db001dc99a47738bbb0f06b475f1a8ce8aab
heringd <dennis.hering@nexum.de>
2015-05-30 00:05:44 +0200

    optimize facts

--- src/test/java/org/puppetory/factories/ReportFactoryTest.java
+++ src/test/java/org/puppetory/factories/ReportFactoryTest.java
@@ -6,6 +6,7 @@ import org.puppetory.dummies.CollectionDummies;
 import org.puppetory.model.api.Filter;
 import org.puppetory.model.api.Inventory;
 import org.puppetory.model.api.Report;
+import org.puppetory.model.impl.FilterImpl;
 import org.puppetory.model.impl.InventoryImpl;
 
 import static org.junit.Assert.assertEquals;
@@ -20,27 +21,26 @@ public class ReportFactoryTest {
 		Inventory inventory = mock(InventoryImpl.class);
 		when(inventory.find(any(String.class), any(Filter.class))).thenReturn(CollectionDummies.getSimpleCollection());
 		
-		factory = new ReportFactory(inventory);
+		factory = new ReportFactory("org.puppetory.report.TestOverviewReport");
 		factory.setFilterString("");
-		factory.setReportClass("org.puppetory.report.TestOverviewReport");
+        factory.setInventory(inventory);
 		factory.setName("server");
-		factory.afterPropertiesSet();
 	}
 
 	@Test
 	public void tesGetObject() throws Exception {
 		
-		Report report = factory.getObject();
+		Report report = factory.createReport();
 		
 		assertEquals("[name: 'dummyFact': [name: dummyFact][foo: bar]]", report.toString());
 	}
 	
 	@Test
 	public void tesGetObjectType() throws Exception {
-		
-		Class<?> objectType = factory.getObjectType();
-		
-		assertEquals("org.puppetory.report.TestOverviewReport", objectType.getName());
+
+        Report report = factory.createReport(new FilterImpl(""));
+
+        assertEquals("[name: 'dummyFact': [name: dummyFact][foo: bar]]", report.toString());
 	}
 	
 }
