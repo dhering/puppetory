c74902c95ec82de1791a7c408ef22d6144eec0f0
heringd <dennis.hering@nexum.de>
2015-05-30 00:08:51 +0200

    add filter option and error handling

--- src/main/java/org/puppetory/web/WebReportController.java
+++ src/main/java/org/puppetory/web/WebReportController.java
@@ -1,6 +1,9 @@
 package org.puppetory.web;
 
+import org.bson.Document;
+import org.puppetory.factories.ReportFactory;
 import org.puppetory.model.api.Report;
+import org.puppetory.model.impl.FilterImpl;
 import org.puppetory.web.exceptions.ResourceNotFoundException;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebReportController implements ApplicationContextAware{
@@ -22,11 +26,27 @@ public class WebReportController implements ApplicationContextAware{
 	}
 	
 	@RequestMapping("/report/{reportId}")
-	public String showReport(@PathVariable String reportId, Model model) throws ResourceNotFoundException{
+	public String showReport(@PathVariable String reportId,
+                             @RequestParam(required = false) String searchFilter,
+                             Model model) throws Exception {
 
 		try {
-			Report report = (Report) applicationContext.getBean(reportId);
+            ReportFactory reportFactory = (ReportFactory) applicationContext.getBean(reportId);
+            Report report;
+            if(searchFilter == null || searchFilter.isEmpty()){
+                report = reportFactory.createReport();
+            }
+            else if(validateFilter(searchFilter)){
+                report = reportFactory.createReport(new FilterImpl(searchFilter));
+            } else {
+                report = reportFactory.createReport();
+                model.addAttribute("searchMsg", "Invalid search query: '" + searchFilter + "'. <br />" +
+                                "Try to use a format like: '{name: \"searchquery\"}'");
+            }
+
+			model.addAttribute("searchFilter", searchFilter);
 			model.addAttribute("report", report.toString());
+			model.addAttribute("reportUrl", "/report/" + reportId);
 		} catch (BeansException e) {
             e.printStackTrace();
 			throw new ResourceNotFoundException();
@@ -34,6 +54,23 @@ public class WebReportController implements ApplicationContextAware{
 				
 		return "webReport";
 	}
+
+    private boolean validateFilter(String searchFilter){
+
+        if(searchFilter == null || searchFilter.isEmpty()){
+            return false;
+        }
+
+        Document document = null;
+
+        try{
+            document = Document.parse(searchFilter);
+        } catch (Exception e){
+            return false;
+        }
+
+        return document == null ? false : true;
+    }
 
 	
 }
