402f726eeb13ce4e57afece50bcfdce9201a6a40
heringd <dennis.hering@nexum.de>
2015-05-29 23:59:48 +0200

    add comments

--- vagrant/scripts/deploy-puppetory.sh
+++ vagrant/scripts/deploy-puppetory.sh
@@ -2,8 +2,13 @@
 
 cd /var/puppetory/
 
+# build project
 mvn clean install
 
+# copy configuration to /etc
+cp /var/puppetory/config/puppetory.properties /etc/
+
+# stop tomcat server, replace WAR file and start tomcat again
 service tomcat7 stop
 
 if [ -d /var/lib/tomcat7/webapps/ROOT ]; then
