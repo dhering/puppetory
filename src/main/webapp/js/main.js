4ba186ec0f251bde2604e02dd56ad1beacd74ab0
heringd <dennis.hering@nexum.de>
2015-05-31 21:08:52 +0200

    avoid click action

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,4 +1,5 @@
 $(".toggle-details").click(function(){
     $(this).parent().parent().toggleClass("panel-primary panel-default");
     $(this).parent().parent().find(".panel-body").toggleClass("hide");
+    return false;
 });
