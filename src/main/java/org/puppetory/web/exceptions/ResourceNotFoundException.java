002cb1286aac9a5292d942f3594b45b1dcbfee24
heringd <dennis.hering@nexum.de>
2015-06-09 22:19:56 +0200

    add constructor

--- src/main/java/org/puppetory/web/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/puppetory/web/exceptions/ResourceNotFoundException.java
@@ -5,4 +5,8 @@ package org.puppetory.web.exceptions;
  */
 public class ResourceNotFoundException extends Exception {
 	private static final long serialVersionUID = -5137152214769129559L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
